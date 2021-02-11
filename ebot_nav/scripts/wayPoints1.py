fa3d779a89c5fcc259c02a9156a47586e887a08a
SaikumarDande <saikumardande2001@gmail.com>
2021-02-11 23:34:21 +0530

    Changes to wappoints

--- ebot_nav/scripts/wayPoints1.py
+++ ebot_nav/scripts/wayPoints1.py
@@ -1,9 +1,250 @@
 #! /usr/bin/env python
 
-import rospy
-import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from geometry_msgs.msg import Pose
+
+import rospy
+import sys
+import copy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import actionlib
+import math
+import tf
+from tf.transformations import euler_from_quaternion
+from tf.transformations import quaternion_from_euler
+
+from moveit_msgs.msg import CollisionObject, AttachedCollisionObject
+from moveit_ros_planning_interface import _moveit_planning_scene_interface
+from geometry_msgs.msg import PoseStamped, Pose, Point
+from object_msgs.msg import ObjectPose
+from shape_msgs.msg import SolidPrimitive, Plane, Mesh, MeshTriangle
+import pyassimp
+
+class Ur5Moveit:
+
+    # Constructor
+    def __init__(self):
+
+        rospy.init_node('node_eg3_set_joint_angles', anonymous=True)
+
+        self._planning_group = "ur5_1_planning_group"
+        self._commander = moveit_commander.roscpp_initialize(sys.argv)
+        self._robot = moveit_commander.RobotCommander()
+        self._scene = moveit_commander.PlanningSceneInterface()
+        self._group = moveit_commander.MoveGroupCommander(self._planning_group)
+        self.gripper = moveit_commander.MoveGroupCommander("hand")
+        self._display_trajectory_publisher = rospy.Publisher(
+            '/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=1)
+
+        self._exectute_trajectory_client = actionlib.SimpleActionClient(
+            'execute_trajectory', moveit_msgs.msg.ExecuteTrajectoryAction)
+        self._exectute_trajectory_client.wait_for_server()
+
+        self._planning_frame = self._group.get_planning_frame()
+        self._eef_link = self._group.get_end_effector_link()
+        self._group_names = self._robot.get_group_names()
+        self._box_name = ''
+
+        # Current State of the Robot is needed to add box to planning scene
+        self._curr_state = self._robot.get_current_state()
+
+        rospy.loginfo(
+            '\033[94m' + "Planning Group: {}".format(self._planning_frame) + '\033[0m')
+        rospy.loginfo(
+            '\033[94m' + "End Effector Link: {}".format(self._eef_link) + '\033[0m')
+        rospy.loginfo(
+            '\033[94m' + "Group Names: {}".format(self._group_names) + '\033[0m')
+
+        rospy.loginfo('\033[94m' + " >>> Ur5Moveit init done." + '\033[0m')
+
+        scene = moveit_commander.PlanningSceneInterface()
+        # ROS answers suggested adding a sleep here to allow the PlanningSceneInterface
+        # to initialize properly; we have noticed that collision objects aren't
+        # always received without it.
+
+        self._psi = _moveit_planning_scene_interface.PlanningSceneInterface()
+        self._pub_co = rospy.Publisher('/collision_object', CollisionObject, queue_size=100)
+        self._pub_aco = rospy.Publisher('/attached_collision_object', AttachedCollisionObject, queue_size=100)
+
+        rospy.sleep(2.0)
+
+    def go_to_pose(self, arg_pose):
+
+        pose_values = self._group.get_current_pose().pose
+        #rospy.loginfo('\033[94m' + ">>> Current Pose:" + '\033[0m')
+        #rospy.loginfo(pose_values)
+#
+        #angles = euler_from_quaternion([pose_values.orientation.x, pose_values.orientation.y,pose_values.orientation.z,pose_values.orientation.w])
+        #print("CURRENT ANGLE")
+        #rospy.loginfo(angles)
+
+        self._group.set_pose_target(arg_pose)
+        flag_plan = self._group.go(wait=True)  # wait=False for Async Move
+
+        #pose_values = self._group.get_current_pose().pose
+        #rospy.loginfo('\033[94m' + ">>> Final Pose:" + '\033[0m')
+        #rospy.loginfo(pose_values)
+#
+        #list_joint_values = self._group.get_current_joint_values()
+        #rospy.loginfo('\033[94m' + ">>> Final Joint Values:" + '\033[0m')
+        #rospy.loginfo(list_joint_values)
+
+        if (flag_plan == True):
+            rospy.loginfo(
+                '\033[94m' + ">>> go_to_pose() Success" + '\033[0m')
+        else:
+            rospy.logerr(
+                '\033[94m' + ">>> go_to_pose() Failed. Solution for Pose not Found." + '\033[0m')
+
+        return flag_plan
+    def add_box(self, name, pose, size = (0.1, 0.1, 0.1)):
+        """
+        Add a box to the planning scene
+        """
+        self._pub_co.publish(self.make_box(name, pose, size))
+
+    def make_box(self, name, pose, size):
+        co = CollisionObject()
+        co.operation = CollisionObject.ADD
+        co.id = name
+        co.header = pose.header
+        box = SolidPrimitive()
+        box.type = SolidPrimitive.BOX
+        box.dimensions = list(size)
+        co.primitives = [box]
+        co.primitive_poses = [pose.pose]
+        return co
+
+    def add_mesh(self, name, pose, filename, size = (1, 1, 1)):
+        self._pub_co.publish(self.__make_mesh(name, pose, filename, size))
+
+    def __make_mesh(self, name, pose, filename, scale = (1, 1, 1)):
+        co = CollisionObject()
+        scene = pyassimp.load(filename)
+        if not scene.meshes:
+            raise MoveItCommanderException("There are no meshes in the file")
+        co.operation = CollisionObject.ADD
+        co.id = name
+        co.header = pose.header
+
+        mesh = Mesh()
+        for face in scene.meshes[0].faces:
+            triangle = MeshTriangle()
+            if len(face) == 3:
+                triangle.vertex_indices = [face[0], face[1], face[2]]
+            mesh.triangles.append(triangle)
+        for vertex in scene.meshes[0].vertices:
+            point = Point()
+            point.x = vertex[0]*scale[0]
+            point.y = vertex[1]*scale[1]
+            point.z = vertex[2]*scale[2]
+            mesh.vertices.append(point)
+        co.meshes = [mesh]
+        co.mesh_poses = [pose.pose]
+        pyassimp.release(scene)
+        return co
+
+    def go_to_joint(self, joint_location):
+        self._group.set_joint_value_target(joint_location) #[1.1, -0.9, 1.28, -1.92, -1.54, -0.21]
+        #self._group.plan()
+        flag_plan = self._group.go(wait=True)
+
+    def closeGripper(self, temp):
+        #self.gripper.set_named_target("close")
+        #temp = float(input("Enter how much to close: "))
+        self.gripper.set_joint_value_target({'gripper_finger1_joint': temp})
+        flag_close = self.gripper.go(wait=True)
+        if flag_close:
+            rospy.loginfo(
+                '\033[94m' + ">>> go_to_pose() Success" + '\033[0m')
+        else:
+            rospy.logerr(
+                '\033[94m' + ">>> go_to_pose() Failed. Cannot close" + '\033[0m')
+
+        return flag_close
+
+    def go_to_joint(self, joint_location):
+        self._group.set_joint_value_target(joint_location) #[1.1, -0.9, 1.28, -1.92, -1.54, -0.21]
+        flag_plan = self._group.go(wait=True)
+
+    def openGripper(self):
+        self.gripper.set_named_target("open")
+        flag_open = self.gripper.go(wait=True)
+        if flag_open:
+            rospy.loginfo(
+                '\033[94m' + ">>> go_to_pose() Success" + '\033[0m')
+        else:
+            rospy.logerr(
+                '\033[94m' + ">>> go_to_pose() Failed. Cannot close" + '\033[0m')
+
+        return flag_open
+
+    # Destructor
+    def __del__(self):
+        moveit_commander.roscpp_shutdown()
+        rospy.loginfo(
+            '\033[94m' + "Object of class Ur5Moveit Deleted." + '\033[0m')
+
+def findObjects():
+    listener = tf.TransformListener()
+    coke, glue, battery = False, False, False
+    tran_coke, tran_glue, tran_battery = 0, 0, 0
+    rate = rospy.Rate(10.0)
+    obj_name = '/object_'
+    while not rospy.is_shutdown():
+        for i in range(25, 40):
+            try:
+                obj_id = obj_name + str(i)
+                (tran1, _) = listener.lookupTransform('/ebot_base', obj_id, rospy.Time(0))
+                (tran2, _) = listener.lookupTransform('/base_link', obj_id, rospy.Time(0))
+                if 25<=i<=29:
+                    tran_coke1 = tran1
+                    tran_coke2 = tran2
+                    coke = True
+                elif 34<=i<=37:
+                    tran_battery1 = tran1
+                    tran_battery2 = tran2
+                    battery = True
+                elif 30<=i<=33 or i==39:
+                    tran_glue1 = tran1
+                    tran_glue2 = tran2
+                    glue = True
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+        if coke and glue and battery:
+            break
+        rate.sleep()
+    return tran_coke1, tran_glue1, tran_battery1, tran_coke2, tran_glue2, tran_battery2
+
+def addObjectBox(name, position, size, tolerance, angle):
+    obj = geometry_msgs.msg.PoseStamped()
+    obj.header.frame_id = "/ebot_base"
+    obj.pose.position.x = position[0]+tolerance[0]
+    obj.pose.position.y = position[1]+tolerance[1]
+    obj.pose.position.z = position[2]+tolerance[2]
+    angles = quaternion_from_euler(angle[0], angle[1], angle[2])
+    obj.pose.orientation.x = angles[0]
+    obj.pose.orientation.y = angles[1]
+    obj.pose.orientation.z = angles[2]
+    obj.pose.orientation.w = angles[3]
+
+    ur5.add_box(name, obj, size)
+
+def get_publish_obj(name, position):
+    obj = ObjectPose()
+    obj.name = name
+    obj.pose.header.frame_id = "/base_link"
+    obj.pose.pose.position.x = position[0]
+    obj.pose.pose.position.y = position[1]
+    obj.pose.pose.position.z = position[2]
+    angles = quaternion_from_euler(0, 0, 0)
+    obj.pose.pose.orientation.x = angles[0]
+    obj.pose.pose.orientation.y = angles[0]
+    obj.pose.pose.orientation.z = angles[0]
+    obj.pose.pose.orientation.w = angles[0]
+    detection_pub.publish(obj)
+
 
 def movebase_client(g):
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
@@ -28,11 +269,33 @@ def movebase_client(g):
 # def move_points(points):
 
 if __name__ == '__main__':
-    way_points = [(-9.1, -1.2, 0.29, 0.96),
-                  (10.7, 10.5,-0.705, 0.71),
-                  (12.6, -1.55, 0.707,0.701),
-                  (18.2, -1.4, 0.923, 0.4),
-                  (-2.0, 4.0, 0.1, 0.1)]
+    # way_points = [(-9.1, -1.2, 0.29, 0.96),
+    #               (10.7, 10.5,-0.705, 0.71),
+    #               (12.6, -1.55, 0.707,0.701),
+    #               (18.2, -1.4, 0.923, 0.4),
+    #               (-2.0, 4.0, 0.1, 0.1)]
+    global ur5
+    ur5 = Ur5Moveit()
+    lst_joint_angles_1 = [0, 0.74, 0.31, -0.02, -0.96, -0.02]
+    ur5.go_to_joint(lst_joint_angles_1)
+
+
+
+
+    way_points = [
+                (14.700, -0.810, -0.718, 0.695), #Pantry Pickup Table 1
+                (11.233, -1.222, -0.709, -0.70), #Pantry Pickup Table 2 Position 1
+                (11.303, -0.910, -0.718, 0.696), #Pantry Pickup Table 2 Position 2
+                ( 7.062, 2.8200, 1.0000, 0.010), #Meeting DropBox
+                ( 7.999, 2.8400, 1.0000, 0.035), #Meeting Pickup
+                (11.400, 10.010, -0.018, -1.00), #Reaserch DropBox along length
+                # (10.930, 9.1800, -0.700, -0.71), #Reaserch DropBox along width
+                (26.165, -2.714, -0.891, 0.454)  #Store Pickup 1
+                (25.920, -3.064, -0.887, 0.462), #Store Pickup 2
+                ( 7.062, 2.8200, 1.0000, 0.010), #Conference DropBox
+                (0,0,0,0,0,0)]                   #Start
+
+
     i = 0
     for goal in way_points:
         try:
@@ -43,3 +306,5 @@ if __name__ == '__main__':
                 rospy.loginfo("Goal execution done! Reached: " + str(i) + " Way-Point, Co-orinates: " + str(goal[0]) + ", "+ str(goal[1]))
         except rospy.ROSInterruptException:
             rospy.loginfo("Navigation test finished.")
+
+    del ur5
